26de62cf8e47ca93abd50923b15a87695e69a49d
Rajib Ahmed <l.rajibahmed@gmail.com>
2016-11-16 21:29:41 +0100

    Add basic routing and bulma.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,14 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
+    <h1 class="title is-1">asdfas asdfs a</h1>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
-
 export default {
-  name: 'app',
-  components: {
-    Hello
-  }
+  name: 'app'
 }
 </script>
-
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import 'styles/main.css'
 </style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,19 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App'
+import Hello from './components/Hello'
+
+const routes = [
+  { path: '/foo', component: Hello }
+]
 
 /* eslint-disable no-new */
 new Vue({
+  routes,
   el: '#app',
   template: '<App/>',
   components: { App }
 })
+
+Vue.use(VueRouter)
+
