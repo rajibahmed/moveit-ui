07aafe243a40d284e944ff8c9df208c582e3cfc8
Rajib Ahmed <l.rajibahmed@gmail.com>
2016-11-17 00:30:55 +0100

    Add navigation toggle option.

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,31 @@
 <template>
-  <div id="app">
-    <h1 class="title is-1">asdfas asdfs a</h1>
-  </div>
+  <section class="hero is-success is-fullheight">
+    <!-- Hero header: will stick at the top -->
+    <div class="hero-head">
+      <navigation></navigation>
+    </div>
+    <!-- Hero content: will be in the middle -->
+    <div class="hero-body">
+      <div class="container has-text-centered">
+        <h1 class="title">
+          Title
+        </h1>
+        <h2 class="subtitle">
+          Subtitle
+        </h2>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
+import Navigation from './components/Navigation'
+
 export default {
-  name: 'app'
+  name: 'app',
+  components: {
+    Navigation
+  }
 }
 </script>
 <style>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,57 @@
+<template>
+  <header class="nav">
+    <div class="container">
+      <div class="nav-left">
+        <a class="nav-item">
+          <img src="../assets/logo.png" alt="Logo">
+        </a>
+      </div>
+      <span :class="menuIcon" @click="toggleMenu()">
+        <span></span>
+        <span></span>
+        <span></span>
+      </span>
+      <div :class="menuItems">
+        <a class="nav-item is-active">
+          Home
+        </a>
+        <a class="nav-item">
+          Examples
+        </a>
+        <a class="nav-item">
+          Documentation
+        </a>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'navigation',
+  data () {
+    return {
+      isActive: false
+    }
+  },
+  methods: {
+    toggleMenu () {
+      this.isActive = !this.isActive
+    }
+  },
+  computed: {
+    menuItems () {
+      return {
+        'nav-right nav-menu is-active': this.isActive,
+        'nav-right nav-menu': !this.isActive
+      }
+    },
+    menuIcon () {
+      return {
+        'nav-toggle is-active': this.isActive,
+        'nav-toggle': !this.isActive
+      }
+    }
+  }
+}
+</script>
